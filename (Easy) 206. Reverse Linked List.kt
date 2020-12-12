469d264ce7d403fcfbb6af6d92031e7d0bc44fc6
KennyxLan <64935509+KennyxLan@users.noreply.github.com>
2020-12-12 22:13:49 +0800

    Update (Easy) 206. Reverse Linked List.kt

--- (Easy) 206. Reverse Linked List.kt
+++ (Easy) 206. Reverse Linked List.kt
@@ -25,4 +25,35 @@ class Solution {
         return ans
     }
 
+    
+//    fun reverseList(head: ListNode?): ListNode? { // recursively
+//        return recursive(head, null)
+//    }
+//    
+//    fun recursive(current: ListNode?, left: ListNode?): ListNode?{
+//        if(current != null){
+//            var temp: ListNode? = current?.next
+//            current?.next = left
+//            if(temp != null){
+//                return recursive(temp, current)
+//            }else if(temp == null){
+//                return current
+//            }
+//        }
+//        return null
+//    }
+    
+    
+    
+//    fun reverseList(head: ListNode?): ListNode? { // iteratively
+//        var leftHead: ListNode? = null
+//        var rightHead: ListNode? = head
+//        while(rightHead != null){
+//            var tempNode: ListNode? = rightHead.next
+//            rightHead.next = leftHead
+//            leftHead = rightHead
+//            rightHead = tempNode
+//        }
+//        return leftHead
+//    }
 }

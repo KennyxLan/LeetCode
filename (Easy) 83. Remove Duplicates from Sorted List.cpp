f9303de18e82bcc6836e82cf58697f47252911bf
KennyxLan <64935509+KennyxLan@users.noreply.github.com>
2020-12-29 13:29:25 +0800

    Update (Easy) 83. Remove Duplicates from Sorted List.cpp

--- (Easy) 83. Remove Duplicates from Sorted List.cpp
+++ (Easy) 83. Remove Duplicates from Sorted List.cpp
@@ -2,10 +2,17 @@
 (Easy) 83. Remove Duplicates from Sorted List.cpp
 
 12/29/2020 12:11
-(iteratively)
+(iterative)
 Runtime: 8 ms, faster than 98.56% of C++ online submissions for Remove Duplicates from Sorted List.
 Memory Usage: 11.9 MB, less than 99.78% of C++ online submissions for Remove Duplicates from Sorted List.
+
+12/29/2020 13:02
+(recursive)
+Runtime: 8 ms, faster than 98.56% of C++ online submissions for Remove Duplicates from Sorted List.
+Memory Usage: 11.8 MB, less than 99.78% of C++ online submissions for Remove Duplicates from Sorted List.
 */
+
+
 
 /**
  * Definition for singly-linked list.
@@ -19,18 +26,33 @@ Memory Usage: 11.9 MB, less than 99.78% of C++ online submissions for Remove Dup
  */
 class Solution {
 public:
-    ListNode* deleteDuplicates(ListNode* head) {
-        ListNode* currentPtr = head;
-        ListNode* nextPtr = nullptr;
-        while(currentPtr != nullptr){
-            int x = currentPtr->val; // or repeatedly call currentPtr->val in the inner loop test condition
-            nextPtr = currentPtr->next;
-            while(nextPtr != nullptr && nextPtr->val == x){
-                nextPtr = nextPtr->next;
-            }
-            currentPtr->next = nextPtr;
-            currentPtr = nextPtr;
-        }
-        return head;
+    ListNode* deleteDuplicates(ListNode* head) { // recursive
+        return recursive(head, head);
+    }
+    
+    ListNode* recursive(ListNode* h, ListNode* currentPtr){
+        if(currentPtr == nullptr || currentPtr->next == nullptr)
+            return h;
+        else if(currentPtr->val == (currentPtr->next)->val){
+            currentPtr->next = (currentPtr->next)->next;
+            return recursive(h, currentPtr);
+        }else
+            return recursive(h, currentPtr->next);
     }
 };
+    
+//    ListNode* deleteDuplicates(ListNode* head) {  // iterative
+//        ListNode* currentPtr = head;
+//        ListNode* nextPtr = nullptr;
+//        //int x;
+//        while(currentPtr != nullptr){
+//            int x = currentPtr->val;
+//            nextPtr = currentPtr->next;
+//            while(nextPtr != nullptr && nextPtr->val == x){
+//                nextPtr = nextPtr->next;
+//            }
+//            currentPtr->next = nextPtr;
+//            currentPtr = nextPtr;
+//        }
+//        return head;
+//    }
